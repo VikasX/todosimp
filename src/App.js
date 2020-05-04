719ba551b7db1d21dad465a5a386872936807ab9
Vikas M <mishra7vikas@gmail.com>
2020-05-04 20:52:04 +0530

    new addition

--- src/App.js
+++ src/App.js
@@ -6,10 +6,34 @@ import Clascomp from './components/Classcomp';
 
 class App extends Component {
   
+  constructor(){
+    super();
+
+    this.state={
+      term : "abc"
+    }
+    
+ this.changehandle=this.changehandle.bind(this);
+
+  }
+
+changehandle(e){
+this.setState( {term : e.target.value})
+
+}
+
+buthandle(e){
+
+}
+
+
   render(){
     return (
       <div className="App">
-  
+        <form>
+          <input type="text" onChange={this.changehandle} value={this.state.term}/>
+          <button onClick={() => this.buthandle.bind(this)}>Click</button>
+        </form>
         <Greet name="vikas"/>
         <Clascomp/>
       </div>
